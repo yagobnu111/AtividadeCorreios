30e7d153b7e7c1109e38e27fd9f9886d360e74dd
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-10-24 17:19:41 -0300

    mudanças

--- AtividadeCorreios/AtividadeCorreios/Scripts/Utils/scripts.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/scripts.js
@@ -1,32 +1,16 @@
-﻿$(document).ready(
-        function () {
-            $('button[name="btnconsulta"]').click(
+﻿$(document).ready(function () {
+   $('button[name="btnconsulta"]').click(function () {
+         var information = $('form[name="frm"]').serializeArray();
+         var obterAtributo = $('form[name="frm"]').attr('send-post');
 
-                function () {
-                    var cepOrigem = $('input[name="cepori"]').val();
-                    var cepDestino = $('input[name="cepdes"]').val();
-                    var peso = $('input[name="peso"]').val();
-                    var comprimento = $('input[name="comprimento"]').val();
-                    var altura = $('input[name="altura"]').val();
-                    var largura = $('input[name="largura"]').val();
-                    var valorDeclarado = $('input[name="valordeclarado"]').val();
-
-                    var url = "http://usysweb.com.br/api/correiosambev.php";
-                    var info = `nCdEmpresa=08082650&sDsSenha=564321&sCepOrigem=${cepOrigem}&sCepDestino=${cepDestino}&nVlPeso=${peso}&nCdFormato=1&nVlComprimento=${comprimento}&nVlAltura=${altura}&nVlLargura=${largura}&sCdMaoPropria=n&nVlValorDeclarado=${valorDeclarado}&sCdAvisoRecebimento=n&nCdServico=04510&nVlDiametro=0&StrRetorno=xml&nIndicaCalculo=3`;
-                    ;
-
-                    $.get(url, info, function (data) {
-                    var dataJson = JSON.parse(data)
-
-                        var informacoesPorUrl = "newpage.html?preco=" + dataJson.cServico.Valor + "&prazo=" + dataJson.cServico.PrazoEntrega;
-
-                    window.open(informacoesPorUrl);
-
-                       
-
-                    });
-                })
-        });
+       $.post("http://usysweb.com.br/api/" + obterAtributo + "correiosambev.php" + information, function (data) {
+                data = JSON.parse(data);
+    $.each(data.cServico, function (key, value) {
+        $('p[name="{key}"'.replace("{key}", key)).text(value);
+         });
+     });
+   });
+});
 
 
 
