b4e7110ba4452e980c9a55f6b4adf693c6890f33
900091 <900091@BLUE05>
2019-10-24 15:23:21 -0300

    lalalal

--- AtividadeCorreios/AtividadeCorreios/Scripts/Utils/scripts2.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/scripts2.js
@@ -4,7 +4,9 @@ $(document).ready(
         var preco = url.toString().split("?")[1].split("&")[0].split("=")[1];
         var prazo = url.toString().split("?")[1].split("&")[1].split("=")[1];
 
+
         $('input[name="precoResultado"]').val("R$" + preco);
         $('input[name="prazoResultado"]').val(prazo + " dias");
+
     }
 );
