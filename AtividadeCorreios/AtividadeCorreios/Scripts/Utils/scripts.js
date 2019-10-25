dc08c3c94956c356e19ea38844d15f47f7ff42b0
Daniel Viegas Vieira <Danielvieiraviegas@gmail.com>
2019-10-25 11:10:51 -0300

    DESIGN 7

--- AtividadeCorreios/AtividadeCorreios/Scripts/Utils/scripts.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Utils/scripts.js
@@ -3,9 +3,9 @@
          var information = $('form[name="frm"]').serializeArray();
          var obterAtributo = $('form[name="frm"]').attr('send-post');
 
-       $.post("http://usysweb.com.br/api/" + obterAtributo + "correiosambev.php" + information, function (data) {
+       $.post("http://usysweb.com.br/api/" + obterAtributo + ".php?giomar=true" , information, function (data) {
                 data = JSON.parse(data);
-    $.each(data.cServico, function (key, value) {
+        $.each(data.cServico, function (key, value) {
         $('p[name="{key}"'.replace("{key}", key)).text(value);
          });
      });
